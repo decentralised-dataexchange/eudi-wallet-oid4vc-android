f709f7d60b45fbea5d1485deabc96fb69aba57f3
milan <milan@igrant.io>
2024-07-29 21:40:17 +0530

    Fix #48 - Support value by reference in dynamic credential request

--- eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/models/PresentationDefinition.kt
+++ eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/models/PresentationDefinition.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class PresentationDefinition(
 
     @SerializedName("id") var id: String? = null,
+    @SerializedName("name") var name: String? = null,
     @SerializedName("format") var format: Map<String, Jwt>? = mapOf(),
     @SerializedName("input_descriptors") var inputDescriptors: ArrayList<InputDescriptors>? = arrayListOf()
 
