abe85c00dcb7484edd30effb0f936f85dfcfd492
Josep Milan K.A <milan@igrant.io>
2024-11-04 13:39:31 +0530

    Fix: Trim double quotes from disclosures

--- eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/models/WrappedVpTokenResponse.kt
+++ eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/models/WrappedVpTokenResponse.kt
@@ -2,6 +2,7 @@ package com.ewc.eudi_wallet_oidc_android.models
 import com.google.gson.annotations.SerializedName
 data class VPTokenResponse(
     @SerializedName("location") var location: String? = null,
+    @SerializedName("redirect_uri") var redirectUri: String? = null
 )
 
 data class WrappedVpTokenResponse(
