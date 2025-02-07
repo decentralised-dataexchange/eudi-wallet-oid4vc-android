a98d3066c8beb1113f81a207d510c1495ddc56d7
Josep Milan K.A <milan@igrant.io>
2025-02-07 23:33:03 +0530

    Fix: #93 - Version updated to 2025.2.1

--- eudi-wallet-oidc-android/build.gradle.kts
+++ eudi-wallet-oidc-android/build.gradle.kts
@@ -74,7 +74,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.decentraliseddataexchange"
             artifactId = "eudi-wallet-oidc-android"
-            version = "2025.1.4"
+            version = "2025.2.1"
 
             afterEvaluate {
                 from(components["release"])
