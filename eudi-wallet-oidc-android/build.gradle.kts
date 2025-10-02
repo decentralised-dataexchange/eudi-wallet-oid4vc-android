e63a5e0555d64f51bbe7c9825b5a27d208539d56
Lijo George <81345608+lijogeorgep@users.noreply.github.com>
2025-10-02 10:14:05 +0530

    Fix: #369 - Version updated to 2025.10.1

--- eudi-wallet-oidc-android/build.gradle.kts
+++ eudi-wallet-oidc-android/build.gradle.kts
@@ -81,7 +81,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.decentraliseddataexchange"
             artifactId = "eudi-wallet-oidc-android"
-            version = "2025.9.6"
+            version = "2025.10.1"
 
             afterEvaluate {
                 from(components["release"])
