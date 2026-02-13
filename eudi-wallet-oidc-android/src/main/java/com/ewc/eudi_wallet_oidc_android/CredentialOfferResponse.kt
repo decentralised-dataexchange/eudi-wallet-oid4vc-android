bae445a908900a5937efccb24e2bc9a7358e0d27
Lijo George <81345608+lijogeorgep@users.noreply.github.com>
2026-02-13 20:53:22 +0530

    Fix: #469 - Added walletUnitAttestation and credentialIssuer parameters in CredentialOfferResponse

--- eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/CredentialOfferResponse.kt
+++ eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/CredentialOfferResponse.kt
@@ -3,5 +3,7 @@ package com.ewc.eudi_wallet_oidc_android
 import com.google.gson.annotations.SerializedName
 
 data class CredentialOfferResponse(
-    @SerializedName("credentialOffer") var credentialOffer: String? = null
+    @SerializedName("credentialOffer") var credentialOffer: String? = null,
+    @SerializedName("walletUnitAttestation") var walletUnitAttestation: String? = null,
+    @SerializedName("credentialIssuer") var credentialIssuer: String? = null,
 )
