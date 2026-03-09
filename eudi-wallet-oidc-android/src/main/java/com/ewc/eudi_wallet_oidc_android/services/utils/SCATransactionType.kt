38af5d68571cd31e8f9768596f111ed5c9933f79
Lijo George <81345608+lijogeorgep@users.noreply.github.com>
2026-03-09 19:05:14 +0530

    Fix #477: Added support for account access and emandate type verification

--- eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/services/utils/SCATransactionType.kt
+++ eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/services/utils/SCATransactionType.kt
@@ -2,11 +2,15 @@ package com.ewc.eudi_wallet_oidc_android.services.utils
 
 enum class SCATransactionType(val type: String) {
     PAYMENT("urn:eudi:sca:payment:1"),
-    LOGIN_RISK("urn:eudi:sca:login_risk_transaction:1");
+    LOGIN_RISK("urn:eudi:sca:login_risk_transaction:1"),
+    ACCOUNT_ACCESS("urn:eudi:sca:account_access:1"),
+    EMANDATE("urn:eudi:sca:emandate:1");
 
     companion object {
         fun isPayment(type: String?) = type == PAYMENT.type
         fun isLoginRisk(type: String?) = type == LOGIN_RISK.type
+        fun isAccountAccess(type: String?) = type == ACCOUNT_ACCESS.type
+        fun isEmandate(type: String?) = type == EMANDATE.type
         fun isValidScaType(type: String?): Boolean {
             return entries.any { it.type == type }
         }
