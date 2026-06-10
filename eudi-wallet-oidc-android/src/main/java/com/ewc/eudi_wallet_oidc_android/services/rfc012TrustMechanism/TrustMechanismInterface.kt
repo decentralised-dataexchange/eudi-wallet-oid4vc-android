a869b27d3bc62e7bf5bcb5e08d3be5c76b838d4e
Lijo George <lachulijo3@gmail.com>
2026-06-10 14:41:52 +0530

    Fix #597 - Updated trust validation to use TrustServiceProvider list instead of URLs

--- eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/services/rfc012TrustMechanism/TrustMechanismInterface.kt
+++ eudi-wallet-oidc-android/src/main/java/com/ewc/eudi_wallet_oidc_android/services/rfc012TrustMechanism/TrustMechanismInterface.kt
@@ -5,11 +5,13 @@ import com.ewc.eudi_wallet_oidc_android.models.TrustServiceProvider
 interface TrustMechanismInterface {
     suspend fun isIssuerOrVerifierTrusted(
         url: String? = null,
-        x5c: String? = null
+        x5c: String? = null,
+        trustProvidersList: List<TrustServiceProvider>? = null
     ): Boolean
 
     suspend fun fetchTrustDetails(
         url: String? = null,
-        x5c: String? = null
+        x5c: String? = null,
+        trustProvidersList: List<TrustServiceProvider>? = null
     ): TrustServiceProvider?
 }
