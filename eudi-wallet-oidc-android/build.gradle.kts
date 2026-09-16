e713506c74bd13f598bbe9d213c1b76fc6c2004d
Lijo George <lachulijo3@gmail.com>
2026-09-16 17:12:31 +0530

    Fix #695: Version updated to 2026.9.3

--- eudi-wallet-oidc-android/build.gradle.kts
+++ eudi-wallet-oidc-android/build.gradle.kts
@@ -81,7 +81,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.decentraliseddataexchange"
             artifactId = "eudi-wallet-oidc-android"
-            version = "2026.9.2"
+            version = "2026.9.3"
 
             afterEvaluate {
                 from(components["release"])
